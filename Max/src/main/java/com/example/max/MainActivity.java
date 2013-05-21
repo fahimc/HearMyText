d36975defd9a830958cae01c473bd69b740b13c2
Fahim Chowdhury <fahim.chowdhury@ogilvy.com>
2013-05-21 15:30:27 +0100

    init

---
+++ Max/src/main/java/com/example/max/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.max;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.*;
+
+import java.util.Locale;
+
+public class MainActivity extends Activity implements TextToSpeech.OnInitListener  {
+    private ListView listView1;
+    private EditText textbox;
+    private Button sendButton;
+    private TextToSpeech tts;
+    private EditText mobileNumber;
+    private Button clearButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tts = new TextToSpeech(this, this);
+        listView1 = (ListView) findViewById(R.id.listView);
+       textbox = (EditText) findViewById(R.id.editText);
+        mobileNumber = (EditText) findViewById(R.id.mobileNumber);
+        sendButton = (Button) findViewById(R.id.button);
+        clearButton = (Button) findViewById(R.id.clearButton);
+
+        String[] items = { "Hello", "How are you?", "Call me", "Where are you?", "I'm Home", "come", "you", "ok","what", "time", "go","shopping","no","yes","please","thank you","happy birthday","good bye","i don't know","problem","don't","i" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+
+            listView1.setAdapter(adapter);
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+                Object o = listView1.getItemAtPosition(position);
+                String msg = textbox.getText().toString();
+                textbox.setText(msg+" "+o.toString());
+                speakOut();
+
+            }
+        });
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textbox.setText("");
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNumber = mobileNumber.getText().toString();
+                String toastMsg = "";
+                if(phoneNumber == null || phoneNumber.isEmpty())
+                {
+                    toastMsg="Provide a Mobile Number";
+                }else{
+                    SmsManager sms = SmsManager.getDefault();
+                    sms.sendTextMessage(phoneNumber, null, textbox.getText().toString(), null, null);
+                    toastMsg="Sent Message";
+                }
+                Toast msg = Toast.makeText(MainActivity.this, toastMsg, Toast.LENGTH_LONG);
+
+                msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2, msg.getYOffset() / 2);
+
+                msg.show();
+
+            }
+        });
+    }
+    private void speakOut() {
+
+        String text = textbox.getText().toString();
+
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+              //  sendButton.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+}
